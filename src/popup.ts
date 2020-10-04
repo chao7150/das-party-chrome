ba9217c3bf3a634e2717c6d923265aa5d8f9eccc
Satoru Yokoyama <22640673+chao7150@users.noreply.github.com>
2020-10-05 00:58:08 +0900

    feat(popup): roomidを二重に付与しない

--- src/popup.ts
+++ src/popup.ts
@@ -7,7 +7,16 @@ if (!createRoomButton) {
 
 createRoomButton.onclick = (ev: MouseEvent) => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.executeScript(tabs[0].id!, {
+    const tab = tabs[0];
+    if (!tab?.url) {
+      return undefined;
+    }
+    // 既にroomidがついているときroomidを付与しない
+    const url = new URL(tab.url);
+    if (url.searchParams.has("roomid")) {
+      return undefined;
+    }
+    chrome.tabs.executeScript(tab.id!, {
       code: 'location.href = location.href + "&roomid=hoge";',
     });
   });
